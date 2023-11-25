0696ae2139e984fe7fa8d74ffaadae83e95af8e0
klaymant <clement.gauthier@protonmail.com>
2023-11-25 15:00:14 +0100

    Improved volume design

--- src/app/components/GameSession/GameSession.tsx
+++ src/app/components/GameSession/GameSession.tsx
@@ -2,6 +2,9 @@ import { useBlindtestContext } from "@/app/contexts/BlindtestProvider";
 import TrackDisplay from "../TrackDisplay/TrackDisplay";
 import { useTrackDisplay } from "../TrackDisplay/useTrackDisplay";
 import { TracksProvider } from "@/app/contexts/TracksProvider";
+import Image, { StaticImageData } from "next/image";
+import volumeIcon from '../../../../public/volume.png';
+import muteIcon from '../../../../public/mute.png';
 
 function GameSession() {
   const { score, lives, soundOptions, setSoundOptions } = useBlindtestContext();
@@ -27,13 +30,28 @@ function GameSession() {
         <section id="play-data">
           <p id="score">Score: <span>{score}</span></p>
           <p id="lives">Lives: <span>{lives}</span></p>
-          <button onClick={mute}>{audioPreview?.muted ? 'Unmute' : 'Mute'}</button>
-          <input type="range" min="0" max="100" step="1" value={(audioPreview?.volume || 0) * 100} onChange={changeVolume} />
+          <section id="audio-controls">
+            <button
+              type="button"
+              className="option"
+              onClick={mute}
+            >
+              {audioPreview?.muted ?
+                VolumeIcon(muteIcon, 'mute icon') :
+                VolumeIcon(volumeIcon, 'volume icon')
+              }
+            </button>
+            <input type="range" min="0" max="100" step="1" value={(audioPreview?.volume || 0) * 100} onChange={changeVolume} />
+          </section>
         </section>
         <TrackDisplay />
       </TracksProvider>
     </>
   )
+}
+
+function VolumeIcon(src: StaticImageData, alt: string) {
+  return <Image src={src} width={12} height={12} alt={alt} />;
 }
 
 export { GameSession };
