a0ec602cde855259b75234bcbb70ecc0d9d351b0
klaymant <clement.gauthier@protonmail.com>
2023-11-28 22:45:15 +0100

    Changed fonts and background color gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,21 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
 import './globals.css';
 import './range-input.css';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Blindtest Hero',
   description: 'Become the hero of your next blindtest!',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
+
+type RootLayoutProps = {
+  children: ReactNode;
+};
