18bf3eaf8dbccd272ad5ca5f90cb6489e92f3928
klaymant <clement.gauthier@protonmail.com>
2023-11-28 23:41:07 +0100

    Made UI more flashy with box shadows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { Blindtest } from './components/Blindtest/Blindtest';
 export default function Home() {
   return (
     <main>
-      <h1>Blindtest Hero</h1>
-      <div>
-        <Blindtest />
+      <div id="title-container">
+        <h1>Blindtest Hero</h1>
       </div>
+      <Blindtest />
     </main>
   )
 }
