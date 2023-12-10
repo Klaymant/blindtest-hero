5b1da3f138bc9309c7f9c35ce208c78e8493250a
klaymant <clement.gauthier@protonmail.com>
2023-12-10 20:59:15 +0100

    Added some explanation of the game's operation

--- src/app/components/ScreenTitle/ScreenTitle.tsx
+++ src/app/components/ScreenTitle/ScreenTitle.tsx
@@ -9,7 +9,13 @@ function ScreenTitle() {
 
   return (
     <section id="screen-title">
-      <button type="button" onClick={playBlindtest}>Play</button>
+      <div>
+        <h2>It's time for you to be a blindtest hero!</h2>
+        <p>Challenge yourself on your musical skills by picking the right song in each round.</p>
+        <p>When you're good, your score goes up, otherwise you lose a life.</p>
+        <p>Do your best to get the best score!</p>
+        <button type="button" onClick={playBlindtest}>Play</button>
+      </div>
     </section>
   );
 }
