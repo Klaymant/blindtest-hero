3a7498d22ec77dcc460fdd3a0797d877533e149e
klaymant <clement.gauthier@protonmail.com>
2024-02-13 14:02:08 +0100

    Corrected a typo in screen title text

--- src/app/components/ScreenTitle/ScreenTitle.tsx
+++ src/app/components/ScreenTitle/ScreenTitle.tsx
@@ -12,7 +12,7 @@ function ScreenTitle() {
       <div>
         <h2>It's time for you to be a blindtest hero!</h2>
         <p>Challenge yourself on your musical skills by picking the right song in each round.</p>
-        <p>When you're good, your goes to the next round, otherwise you lose a life.</p>
+        <p>When you're good, you go to the next round, otherwise you lose a life.</p>
         <p>Do your best to reach the biggest round!</p>
         <button type="button" className="game-navigation" onClick={playBlindtest}>Play</button>
       </div>
