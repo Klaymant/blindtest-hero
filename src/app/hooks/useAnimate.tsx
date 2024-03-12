ea429e171168b2185b8d08883d5cd52dc53def4d
klaymant <clement.gauthier@protonmail.com>
2024-03-12 20:27:27 +0100

    Removed event listener for useAnimate on cleanup

--- src/app/hooks/useAnimate.tsx
+++ src/app/hooks/useAnimate.tsx
@@ -5,13 +5,16 @@ export function useAnimate(animationName: string): [React.RefObject<HTMLElement>
 
   function animationCallback() {
     ref?.current?.classList?.add(animationName);
-    ref?.current?.addEventListener('animationend', () => {
-      ref?.current?.classList?.remove(animationName);
-    });
-
+    ref?.current?.addEventListener('animationend', animationEndCallback);
+    
     return () => {
       ref?.current?.classList?.remove(animationName);
+      ref?.current?.removeEventListener('animationend', animationEndCallback);
     };
+  }
+  
+  function animationEndCallback() {
+    ref?.current?.classList?.remove(animationName);
   }
 
   return [ref, animationCallback];
